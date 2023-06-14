aa27825dfd0fd3f46fec6dd52b0d5668dc7c8f76
teddywafula <wanjala.e@itcomp.org>
2023-06-14 22:43:34 +0300

    add documentation to cart

--- app/Http/Controllers/CartController.php
+++ app/Http/Controllers/CartController.php
@@ -12,6 +12,51 @@ class CartController extends Controller
 {
     /**
      * Add items to cart.
+     *   @OA\Post(
+     *      path="/api/cart/add-to-cart",
+     *      operationId="addToCart",
+     *      tags={"Cart"},
+     *      summary="Create user cart",
+     *      description="Create user cart",
+     *      @OA\RequestBody(
+     *         required=true,
+     *         @OA\JsonContent(
+     *            required={"product_id", "quantity"},
+     *            @OA\Property(property="product_id", type="integer", format="integer", example=1),
+     *            @OA\Property(property="quantity", type="integer", format="integer", example=1),
+     *            @OA\Property(property="cart_id", type="integer", format="integer", example=1),
+     *         ),
+     *      ),
+     *      @OA\Response(
+     *         response=201,
+     *         description="successful operation",
+     *     ),
+     *     @OA\Response(
+     *          response=401,
+     *          description="Unauthenticated",
+     *      ),
+     *      @OA\Response(
+     *          response=403,
+     *          description="Forbidden"
+     *      ),
+     *      @OA\Response(
+     *          response=400,
+     *          description="Bad Request"
+     *      ),
+     *      @OA\Response(
+     *          response=404,
+     *          description="Not found"
+     *      ),
+     *      @OA\Response(
+     *          response=422,
+     *          description="Unprocessable content"
+     *      ),
+     *      @OA\Response(
+     *          response=500,
+     *          description="Internal server error"
+     *      )
+     *  )
+     *
      */
     public function addToCart(CartRequest $request)
     {
@@ -20,6 +65,49 @@ class CartController extends Controller
 
     /**
      * Store a newly created resource in storage.
+     *   @OA\Post(
+     *      path="/api/cart",
+     *      operationId="createCart",
+     *      tags={"Cart"},
+     *      summary="Create user cart",
+     *      description="Create user cart",
+     *      @OA\RequestBody(
+     *         required=true,
+     *         @OA\JsonContent(
+     *            required={"user_id"},
+     *            @OA\Property(property="user_id", type="integer", format="integer", example=1),
+     *         ),
+     *      ),
+     *      @OA\Response(
+     *         response=201,
+     *         description="successful operation",
+     *     ),
+     *     @OA\Response(
+     *          response=401,
+     *          description="Unauthenticated",
+     *      ),
+     *      @OA\Response(
+     *          response=403,
+     *          description="Forbidden"
+     *      ),
+     *      @OA\Response(
+     *          response=400,
+     *          description="Bad Request"
+     *      ),
+     *      @OA\Response(
+     *          response=404,
+     *          description="Not found"
+     *      ),
+     *      @OA\Response(
+     *          response=422,
+     *          description="Unprocessable content"
+     *      ),
+     *      @OA\Response(
+     *          response=500,
+     *          description="Internal server error"
+     *      )
+     *  )
+     *
      */
     public function createCart(CartRequest $request): JsonResponse
     {
@@ -28,6 +116,41 @@ class CartController extends Controller
 
     /**
      * Display the specified resource.
+     *
+     * @OA\Get(
+     *     tags={"Cart"},
+     *     path="/api/cart/{cart}/items",
+     *     operationId="getCartItems",
+     *     summary="Show cart items",
+     *     description="Show cart items",
+     *     @OA\Parameter(name="cart", in="path", description="Id of cart", required=true,
+     *        @OA\Schema(type="integer")
+     *    ),
+     *     @OA\Response(
+     *         response=200,
+     *         description="successful operation",
+     *     ),
+     *     @OA\Response(
+     *          response=401,
+     *          description="Unauthenticated",
+     *      ),
+     *      @OA\Response(
+     *          response=403,
+     *          description="Forbidden"
+     *      ),
+     *      @OA\Response(
+     *          response=400,
+     *          description="Bad Request"
+     *      ),
+     *      @OA\Response(
+     *          response=404,
+     *          description="Not found"
+     *      ),
+     *      @OA\Response(
+     *          response=422,
+     *          description="Unprocessable content"
+     *      )
+     * )
      */
     public function getCart(Request $request)
     {
@@ -37,6 +160,41 @@ class CartController extends Controller
 
     /**
      * Display the specified resource.
+     *
+     * * @OA\Get(
+     *     tags={"Cart"},
+     *     path="/api/cart",
+     *     operationId="getUserCart",
+     *     summary="Get user cart",
+     *     description="Show user cart",
+     *     @OA\Parameter(name="user_id", in="query", description="Id of user", required=true,
+     *        @OA\Schema(type="integer")
+     *    ),
+     *     @OA\Response(
+     *         response=200,
+     *         description="successful operation",
+     *     ),
+     *     @OA\Response(
+     *          response=401,
+     *          description="Unauthenticated",
+     *      ),
+     *      @OA\Response(
+     *          response=403,
+     *          description="Forbidden"
+     *      ),
+     *      @OA\Response(
+     *          response=400,
+     *          description="Bad Request"
+     *      ),
+     *      @OA\Response(
+     *          response=404,
+     *          description="Not found"
+     *      ),
+     *      @OA\Response(
+     *          response=422,
+     *          description="Unprocessable content"
+     *      )
+     * )
      */
     public function getCartItems(Request $request, $id)
     {
@@ -45,6 +203,46 @@ class CartController extends Controller
 
     /**
      * Remove cart item.
+     *
+     * * @OA\Delete (
+     *     tags={"Cart"},
+     *     path="/api/cart/item/{cartItem}",
+     *     operationId="removeCartItem",
+     *     summary="Remove cart item",
+     *     description="remove cart item",
+     *     @OA\Parameter(name="cartItem", in="path", description="Id of cart item", required=true,
+     *        @OA\Schema(type="integer")
+     *    ),
+     *     @OA\Response(
+     *         response=204,
+     *         description="No content",
+     *     ),
+     *     @OA\Response(
+     *          response=401,
+     *          description="Unauthenticated",
+     *      ),
+     *      @OA\Response(
+     *          response=403,
+     *          description="Forbidden"
+     *      ),
+     *      @OA\Response(
+     *          response=400,
+     *          description="Bad Request"
+     *      ),
+     *      @OA\Response(
+     *          response=404,
+     *          description="Not found"
+     *      ),
+     *      @OA\Response(
+     *          response=422,
+     *          description="Unprocessable content"
+     *      ),
+     *      @OA\Response(
+     *          response=500,
+     *          description="Internal server error"
+     *      )
+     * )
+     *
      */
     public function removeCartItem(Request $request, $id)
     {
